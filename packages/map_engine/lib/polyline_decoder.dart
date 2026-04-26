8f8c485cea22176f7402ba21030e8057407efdf6
Mavaebrook <starcheredward0@gmail.com>
2026-04-26 15:32:00 -0500

    Create polyline_decoder.dart

---
+++ packages/map_engine/lib/polyline_decoder.dart
@@ -0,0 +1,28 @@
+List<List<double>> decodePolyline(String encoded) {
+  final List<List<double>> points = [];
+  int index = 0;
+  int lat = 0, lng = 0;
+  while (index < encoded.length) {
+    int b, shift = 0, result = 0;
+    do {
+      b = encoded.codeUnitAt(index++) - 63;
+      result |= (b & 0x1f) << shift;
+      shift += 5;
+    } while (b >= 0x20);
+    final dlat = (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
+    lat += dlat;
+
+    shift = 0;
+    result = 0;
+    do {
+      b = encoded.codeUnitAt(index++) - 63;
+      result |= (b & 0x1f) << shift;
+      shift += 5;
+    } while (b >= 0x20);
+    final dlng = (result & 1) != 0 ? ~(result >> 1) : (result >> 1);
+    lng += dlng;
+
+    points.add([lat / 1e5, lng / 1e5]);
+  }
+  return points;
+}
